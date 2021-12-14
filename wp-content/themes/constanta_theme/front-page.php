71aae22719c13b78b032c84b406935d9c4dc0b9d
evertonmessias <everton.messias@gmail.com>
2021-12-14 18:58:14 -0300

    14-12-2021_18:58:14

--- wp-content/themes/constanta_theme/front-page.php
+++ wp-content/themes/constanta_theme/front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <main id="main">
 
-	<!-- ======= Why Us Section ======= -->
+	<!-- ======= Quadros ======= -->
 	<section id="industria" class="why-us quadros">
 		<div class="container-fluid">
 			<div class="row">
@@ -30,7 +30,106 @@
 			</div>
 		</div>
 	</section>
-	<!-- End Why Us Section -->
+	<!-- End Quadros -->
 
-  </main><!-- End #main -->
+	<!-- ======= TimeLine ======= -->
+	<section id="testimonials" class="testimonials timeline background-red">
+		<div class="container" data-aos="fade-up">
+
+			<div class="section-title">
+				<h2>TIMELINE</h2>
+			</div>
+
+			<div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+				<div class="swiper-wrapper">
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>1952</h3>								
+								<p>Constanta é fundada por 4 alemães, inicia suas atividades fabricando resistores de carvão.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>1969</h3>								
+								<p>Passa a fazer parte do grupo Philips.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>1998</h3>								
+								<p>Empresários locais compram a Constanta do grupo Philips e a planta é transferida para o bairro do Ipiranga, São Paulo/SP.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>2016</h3>								
+								<p>Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>2017</h3>								
+								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>2019</h3>								
+								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>2020</h3>								
+								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+					<div class="swiper-slide">
+						<div class="testimonial-wrap">
+							<div class="testimonial-item">
+								<h3>2021</h3>								
+								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
+								</p>
+							</div>
+						</div>
+					</div><!-- End testimonial item -->
+
+				</div>
+
+				<div class="swiper-pagination"></div>
+			
+			</div>
+		</div>
+	</section><!-- End TimeLine -->
+
+</main><!-- End #main -->
 <?php get_footer(); ?>
