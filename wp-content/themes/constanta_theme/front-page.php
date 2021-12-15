8405d3f34a607be28839a88b45cf29546942ab0a
evertonmessias <everton.messias@gmail.com>
2021-12-15 18:47:39 -0300

    15-12-2021_18:47:39

--- wp-content/themes/constanta_theme/front-page.php
+++ wp-content/themes/constanta_theme/front-page.php
@@ -33,7 +33,7 @@
 	<!-- End Quadros -->
 
 	<!-- ======= TimeLine ======= -->
-	<section id="testimonials" class="testimonials timeline background-red">
+	<section id="timeline" class="testimonials timeline background-red">
 		<div class="container" data-aos="fade-up">
 
 			<div class="section-title">
@@ -47,7 +47,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>1952</h3>								
+								<h3>1952</h3>
 								<p>Constanta é fundada por 4 alemães, inicia suas atividades fabricando resistores de carvão.
 								</p>
 							</div>
@@ -57,7 +57,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>1969</h3>								
+								<h3>1969</h3>
 								<p>Passa a fazer parte do grupo Philips.
 								</p>
 							</div>
@@ -67,7 +67,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>1998</h3>								
+								<h3>1998</h3>
 								<p>Empresários locais compram a Constanta do grupo Philips e a planta é transferida para o bairro do Ipiranga, São Paulo/SP.
 								</p>
 							</div>
@@ -77,7 +77,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>2016</h3>								
+								<h3>2016</h3>
 								<p>Fugiat enim eram quae cillum dolore dolor amet nulla culpa multos export minim fugiat minim velit minim dolor enim duis veniam ipsum anim magna sunt elit fore quem dolore labore illum veniam.
 								</p>
 							</div>
@@ -87,7 +87,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>2017</h3>								
+								<h3>2017</h3>
 								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
 								</p>
 							</div>
@@ -97,7 +97,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>2019</h3>								
+								<h3>2019</h3>
 								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
 								</p>
 							</div>
@@ -107,7 +107,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>2020</h3>								
+								<h3>2020</h3>
 								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
 								</p>
 							</div>
@@ -117,7 +117,7 @@
 					<div class="swiper-slide">
 						<div class="testimonial-wrap">
 							<div class="testimonial-item">
-								<h3>2021</h3>								
+								<h3>2021</h3>
 								<p>Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.
 								</p>
 							</div>
@@ -125,10 +125,74 @@
 					</div><!-- End testimonial item -->
 
 				</div>
-				<div class="swiper-pagination"></div>							
-			</div>					
+				<div class="swiper-pagination"></div>
+			</div>
+		</div>
+		<div class="background-img" data-aos="fade-up" style="background-image: url(<?php echo SITEPATH; ?>assets/img/background/vermelho.jpg)">
+			<div class="row">
+				<div class="col-lg-12 lettering">
+					<img src="<?php echo SITEPATH ?>assets/img/quadros/lettering.gif" title="Lettering">
+				</div>
+			</div>
 		</div>
 	</section><!-- End TimeLine -->
+
+	<!-- ======= Slide-Box ======= -->
+	<section id="slide-box" class="testimonials slide-box background-red">
+		<div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+			<div class="swiper-wrapper">
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro1.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro2.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro3.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro4.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro5.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+
+				<div class="swiper-slide">
+					<div class="testimonial-wrap">
+						<div class="testimonial-item">
+							<img src="<?php echo SITEPATH ?>assets/img/quadros/quadro.png" title="Constanta">
+						</div>
+					</div>
+				</div><!-- End testimonial item -->
+			</div>
+			<div class="swiper-pagination"></div>
+		</div>
+	</section><!-- End Slide-Box -->
 
 </main><!-- End #main -->
 <?php get_footer(); ?>
