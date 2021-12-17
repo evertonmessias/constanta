946cedcc4e9d860bbdadb8fb6fe615aa2a74dd40
evertonmessias <everton.messias@gmail.com>
2021-12-17 16:34:51 -0300

    17-12-2021_16:34:51

--- wp-content/themes/constanta_theme/front-page.php
+++ wp-content/themes/constanta_theme/front-page.php
@@ -21,7 +21,7 @@
 				</div>
 
 				<!-- Slide 2 -->
-				<div class="carousel-item" style="background-image: url(<?php echo SITEPATH; ?>assets/img/slide/slide-2.jpg)">
+				<div class="carousel-item" style="background-image: url(<?php echo SITEPATH; ?>assets/img/slide/slide-2.gif)">
 					<div class="carousel-container">
 						<div class="container">
 							<h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
@@ -32,7 +32,7 @@
 				</div>
 
 				<!-- Slide 3 -->
-				<div class="carousel-item" style="background-image: url(<?php echo SITEPATH; ?>assets/img/slide/slide-3.jpg)">
+				<div class="carousel-item" style="background-image: url(<?php echo SITEPATH; ?>assets/img/slide/slide-3.gif)">
 					<div class="carousel-container">
 						<div class="container">
 							<h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
@@ -40,18 +40,7 @@
 							<a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
 						</div>
 					</div>
-				</div>
-
-				<!-- Slide 4 -->
-				<div class="carousel-item" style="background-image: url(<?php echo SITEPATH; ?>assets/img/slide/slide-4.jpg)">
-					<div class="carousel-container">
-						<div class="container">
-							<h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-							<p class="animate__animated animate__fadeInUp">Lorem Ipsum Dolor</p>
-							<a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-						</div>
-					</div>
-				</div>
+				</div>				
 
 			</div>
 
--- wp-content/themes/constanta_theme/page-sobre.php
+++ wp-content/themes/constanta_theme/page-sobre.php
@@ -17,14 +17,37 @@
 
 <main id="main">
 
-	<!-- ======= Section ======= -->
-	<section id="sobre-frase" class="pricing">	
-			<br><br><br>			
-			<div class="section-title">
-				<h1>DO PIONERISMO DO MERCADO<BR>ELETRÔNICO À EVOLUÇÃO TECNOLÓGICA</h1>				
-			</div>
-			<br>
+	<!-- ======= Section Frase ======= -->
+	<section id="sobre-frase" class="pricing">
+		<br><br><br>
+		<div class="section-title">
+			<h1>DO PIONERISMO DO MERCADO<BR>ELETRÔNICO À EVOLUÇÃO TECNOLÓGICA</h1>
+		</div>
+		<br>
 	</section><!-- End Section -->
+
+	<!-- ======= Section GIF ======= -->
+	<section id="sobre-gif" class="pricing">
+		<img src="<?php echo SITEPATH ?>assets/img/quadros/sobre.gif" title="Constanta">
+	</section><!-- End Section -->
+
+	<!-- ======= Section Mercado ======= -->
+	<section id="sobre-mercado" class="pricing">
+		<div class="container">
+			<br><br>
+			<div class="section-title">				
+				<h2>MERCADO DE ATUAÇÃO</h2>
+				<hr>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					FOTO
+				</div>
+				<div class="col-lg-6">
+					icones
+				</div>				
+			</div>
+		</div>
 
 </main><!-- End #main -->
 <?php get_footer(); ?>
