472d724b49ba9726915ae5f3dacfb077ff0210c6
evertonmessias <everton.messias@gmail.com>
2021-12-20 12:10:44 -0300

    20-12-2021_12:10:44

--- wp-content/themes/constanta_theme/page-sobre.php
+++ wp-content/themes/constanta_theme/page-sobre.php
@@ -32,22 +32,111 @@
 	</section><!-- End Section -->
 
 	<!-- ======= Section Mercado ======= -->
-	<section id="sobre-mercado" class="pricing">
+	<section id="sobre-mercado" class="sobre-mercado pricing section-bg">
 		<div class="container">
 			<br><br>
-			<div class="section-title">				
+			<div class="section-title">
 				<h2>MERCADO DE ATUAÇÃO</h2>
 				<hr>
 			</div>
-			<div class="row">
-				<div class="col-lg-6">
-					FOTO
+			<div class="row box-mercado">
+				<div class="col-lg-6 coluna1">
+					<img class="sobrepersona" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_persona.png" title="Constanta">
 				</div>
-				<div class="col-lg-6">
-					icones
-				</div>				
+				<div class="col-lg-6 coluna2">
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_01.png" title="Constanta">
+						<figcaption>Automotivo</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_02.png" title="Constanta">
+						<figcaption>Energia</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_03.png" title="Constanta">
+						<figcaption>Internet das Coisas (IoT)</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_04.png" title="Constanta">
+						<figcaption>Gestão de Frotas</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_05.png" title="Constanta">
+						<figcaption>Publicidade</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_06.png" title="Constanta">
+						<figcaption>Automação</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_07.png" title="Constanta">
+						<figcaption>Aeroespacial</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_08.png" title="Constanta">
+						<figcaption>Saúde</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_09.png" title="Constanta">
+						<figcaption>Telecomunicações</figcaption>
+					</figure>
+					<figure>
+						<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_10.png" title="Constanta">
+						<figcaption>Agronegócio</figcaption>
+					</figure>
+				</div>
+				<a href="/solucoes"><button class="btnsolucoes" type="button">Soluções</button></a>
 			</div>
 		</div>
+	</section>
+
+	<section id="sobre-mercado" class="sobre-mercado mercado2 pricing" style="background-image: url(<?php echo SITEPATH; ?>assets/img/background/cinza2.jpg)">
+		<div class="container">
+			<div class="row box-mercado">
+				<div class="col-lg-6 coluna2">
+					<h2>POR QUE ESCOLHER<BR>A CONSTANTA ?</h2>
+					<p>
+						A Constanta nasceu em 1952, e nesse período, sua principal atividade era produzir resistores de carvão. Integrou o grupo Phillips durante muitos anos até se estabelecer na cidade de São Paulo e em seguida transferir sua planta fabril para Atibaia, no interior do estado.<br><br>
+
+						Desde sua criação, busca se atualizar e ser referência em manufatura de eletrônicos, atendendo os mais diversos segmentos, com produtos e soluções que vão do agronegócio à área da saúde, com tecnologia de ponta e uma equipe altamente qualificada para atender todas as demandas.<br><br>
+
+						A planta fabril está localizada em Atibaia/SP, próxima a poios de pesquisa e industriais, além de possuir fácil acesso à portos, aeroportos e rodovias, que ligam todo o país e o mundo.<br><br>
+
+						Além disso, a Constanta se preocupa com o meio ambiente e o futuro do planeta, dessa forma, está sempre em busca de melhorias no descarte do lixo eletrônico, promove campanhas de conscientização com a equipe interna, da própria empresa, e externa, das cidades do entorno, além de apoiar diversos projetos sociais.<br><br>
+
+						Constanta é sinônimo de desenvolvimento e qualidade!
+					</p>
+				</div>
+				<div class="col-lg-6 coluna1">
+					<img class="sobrepersona" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_industria.png" title="Constanta">
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="sobre-mercado" class="sobre-mercado mercado2 mercado3 pricing section-bg">
+		<div class="container">
+			<div class="row box-mercado">
+				<div class="col-lg-6 coluna1">
+					<img class="sobreinstituto" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_instituto.png" title="Constanta">
+				</div>
+				<div class="col-lg-6 coluna2">
+					<h2>INSTITUTO CONSTANTA<BR>DE INOVAÇÃO</h2>
+					<h4>Transformando ideias em produtos!</h4>
+					<ul>
+						<li>Pesquisa, desenvolvimento e inovação;</li>
+						<li>Transformação Digital e Evolução tecnológica;</li>
+						<li>Internet das Coisas (loT);</li>
+						<li>Inteligência Artificial;</li>
+						<li>Disrupção dos Mercados;</li>
+						<li>BlockChain;</li>
+					</ul>
+					<a href="/solucoes"><button class="btnsolucoes" type="button">Faça parte da inovação</button></a>
+					<img class="sobreicones" src="<?php echo SITEPATH ?>assets/img/quadros/sobre_icone_11.png" title="Constanta">
+				</div>
+			</div>
+		</div>
+	</section>
 
 </main><!-- End #main -->
 <?php get_footer(); ?>
